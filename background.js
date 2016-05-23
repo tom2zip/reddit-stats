d9308c4746c59c20896c0d2826781faee5bd10eb
Tom <tom2zip@gmail.com>
2016-05-23 00:30:25 -0400

    Timer resets when a different tab is active, have to work on start-up flow

--- background.js
+++ background.js
@@ -1,27 +1,35 @@
-var seconds = 0;
-var timer = {};
-var stats = [];
+const makeTimer = function() {
+	let timerObj = {};
 
-function startTimer() {
-	timer = setInterval(function() {
-		console.log(seconds);
-		seconds++;
-	}, 1000);
-}
+	return {
+		start: function() {
+			timerObj = setInterval(function() {
+				console.log(seconds);
+				seconds++;
+			}, 1000);
+		},
 
-function stopTimer() {
-	clearInterval(timer);
-	seconds = 0;
-}
+	 	reset: function() {
+			clearInterval(timerObj);
+			seconds = 0;
+			this.start();
+		}
+	}
+};
+
+let currUrl = '';
+let stats = [];
+let timer = {};
+let seconds = 0;
 
 function getCurrentTabUrl(callback) {
-	var queryInfo = {
+	const queryInfo = {
 		active: true,
 		currentWindow: true
 	};
 	chrome.tabs.query(queryInfo, function(tabs) {
-		var tab = tabs[0];
-		var url = tab.url;
+		const tab = tabs[0];
+		const url = tab.url;
 		console.assert(typeof url == 'string', 'tab.url should be a string');
 		callback(url);
 	});
@@ -33,36 +41,51 @@ function parseUrl(url) {
 	
 	// starts with http
 	if (httpPart.indexOf('s') === -1) {
-		return url.substr(7);
+		return url.substr(7).split('/')[0];
 	} 
 
 	// starts with https
-	return url.substr(8);
+	return url.substr(8).split('/')[0];
+}
+
+function updateStats(url, seconds) {
+	if (!stats[url]) {
+		stats[url] = seconds;	
+	} else {
+		stats[url] += seconds;
+	}
 }
 
 // reset timer on tab change
 chrome.tabs.onActivated.addListener(function(info) {
 	getCurrentTabUrl(function(url) {
-		console.log('new url: ' + url);
-		if (stats.indexOf(url) === -1) {
-			stats.push(parseUrl(url));
+		const parsedUrl = parseUrl(url);
+		console.log(currUrl, parsedUrl);
+		if (!currUrl || currUrl !== parsedUrl) {
+			currUrl = parsedUrl;
+			updateStats(currUrl, seconds);
+			timer.reset();
+			// if (stats.indexOf(parsedUrl) === -1) {
+			// 	updateStats(parsedUrl, seconds);
+			// 	currUrl = parsedUrl;
+			// 	timer.reset();
+			// }
+			console.log(stats);
 		}
 	});
-
-	// stopTimer();
-	// startTimer();
 });
 
 // start timer in the beginning
 document.addEventListener('DOMContentLoaded', function() {
-	console.log('hello');
-	// getCurrentTabUrl(function(url) {
-	// 	console.log('current tab: ' + url);
-	// });
+	
+	timer = makeTimer();
+	
+	getCurrentTabUrl(function(url) {
+		console.log('current tab: ' + url);
+	});
 	// startTimer();
 });
 
-// receive msg regarding current tab from popup
-chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
-	console.log(msg);
+chrome.runtime.onConnect.addListener(function() {
+	console.log('what up boi');
 });
