8d2a9db94ecdd5385bda84280ab4d5dc6c16d784
Tom <tom2zip@gmail.com>
2016-05-29 22:53:34 -0400

    Fixed lint errors

--- background.js
+++ background.js
@@ -1,47 +1,39 @@
-document.addEventListener('DOMContentLoaded', function() {
-	timer = initTimer();
-	getCurrentTabUrl(function(url) {
-		currUrl = parseUrl(url);
-	});
-	timer.start();
-});
+let currUrl = '';
+let timer = {};
 
 const initTimer = function() {
 	let timerObj = {};
 	let seconds = 0;
 
 	return {
-		start: function() {
-			timerObj = setInterval(function() {
+		start() {
+			timerObj = setInterval(() => {
 				seconds++;
 			}, 1000);
 		},
 
-	 	reset: function() {
+		reset() {
 			clearInterval(timerObj);
 			seconds = 0;
 			this.start();
 		},
 
-		getSeconds: function() {
+		getSeconds() {
 			return seconds;
-		}
-	}
+		},
+	};
 };
-
-let currUrl = '';
-let timer = {};
 
 function getCurrentTabUrl(callback) {
 	const queryInfo = {
 		active: true,
-		currentWindow: true
+		currentWindow: true,
 	};
-	chrome.tabs.query(queryInfo, function(tabs) {
+	chrome.tabs.query(queryInfo, tabs => {
 		// tabs[0] will always be the current tab on current window
 		const tab = tabs[0];
 		const url = tab.url;
-		console.assert(typeof url == 'string', 'tab.url should be a string');
+		// console.assert(typeof url == 'string', 'tab.url should be a string');
 		callback(url);
 	});
 }
@@ -49,11 +41,11 @@ function getCurrentTabUrl(callback) {
 // cuts off the http(s):// part from the url
 function parseUrl(url) {
 	const httpPart = url.substr(0, 5);
-	
+
 	// starts with http
 	if (httpPart.indexOf('s') === -1) {
 		return url.substr(7).split('/')[0];
-	} 
+	}
 
 	// starts with https
 	return url.substr(8).split('/')[0];
@@ -63,13 +55,21 @@ function setStats(url, seconds) {
 	if (!localStorage.getItem(url)) {
 		localStorage.setItem(url, seconds);
 	} else {
-		const localStorageNumValue = parseInt(localStorage.getItem(url));
+		const localStorageNumValue = parseInt(localStorage.getItem(url), 10);
 		localStorage.setItem(url, localStorageNumValue + seconds);
 	}
 }
 
-chrome.tabs.onActivated.addListener(function(info) {
-	getCurrentTabUrl(function(url) {
+document.addEventListener('DOMContentLoaded', () => {
+	timer = initTimer();
+	getCurrentTabUrl((url) => {
+		currUrl = parseUrl(url);
+	});
+	timer.start();
+});
+
+chrome.tabs.onActivated.addListener(() => {
+	getCurrentTabUrl(url => {
 		const parsedUrl = parseUrl(url);
 		if (!currUrl || currUrl !== parsedUrl) {
 			setStats(currUrl, timer.getSeconds());
@@ -79,8 +79,8 @@ chrome.tabs.onActivated.addListener(function(info) {
 	});
 });
 
-chrome.windows.onFocusChanged.addListener(function() {
-	getCurrentTabUrl(function(url) {
+chrome.windows.onFocusChanged.addListener(() => {
+	getCurrentTabUrl(url => {
 		const parsedUrl = parseUrl(url);
 		setStats(currUrl, timer.getSeconds());
 		currUrl = parsedUrl;
--- options.js
+++ options.js
@@ -1,32 +1,34 @@
-document.addEventListener('DOMContentLoaded', function() {
-	console.log('hello');
-	populateStats();
+const stats = [];
 
-	var data = [4, 8, 15, 16, 23, 42];
-	d3.select('.chart')
-		.selectAll('div')
-			.data(data)
-		.enter().append('div')
-			.style('width', function(d) { return d * 10 + 'px'; })
-			.text(function(d) { return d; });
-});
+function convertStatToTime(stat) {
+	const hours = parseInt(stat / 3600, 10);
+	let remainingSeconds = stat - (hours * 3600);
+	const minutes = parseInt(remainingSeconds / 60, 10);
+	remainingSeconds = stat - (hours * 3600) - (minutes * 60);
+	return {
+		hours,
+		minutes,
+		seconds: remainingSeconds,
+	};
+}
 
-let stats = [];
 
 function populateStats() {
-	for (var key in localStorage) {
+	for (const key in localStorage) {
 		stats[key] = convertStatToTime(localStorage[key]);
 	}
 }
 
-function convertStatToTime(stat) {
-	const hours = parseInt(stat / 3600);
-	let remainingSeconds = stat - hours*3600;
-	const minutes = parseInt(remainingSeconds / 60);
-	remainingSeconds = stat - hours*3600 - minutes*60;
-	return {
-		hours: hours,
-		minutes: minutes,
-		seconds: remainingSeconds
-	};
-}
+document.addEventListener('DOMContentLoaded', () => {
+	console.log('hello');
+	populateStats();
+
+	const data = [4, 8, 15, 16, 23, 42];
+	d3.select('.chart')
+		.selectAll('div')
+			.data(data)
+		.enter()
+		.append('div')
+			.style('width', d => `${d * 10}px`)
+			.text(d => d);
+});
