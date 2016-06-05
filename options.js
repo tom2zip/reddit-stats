40d0d8f6da69846d30fb4aea54fa79f330df8de3
Tom <tom2zip@gmail.com>
2016-06-05 18:26:24 -0400

    Can get bars to show in the graph

--- options.js
+++ options.js
@@ -50,38 +50,38 @@ function calculateTopFive(stats) {
 }
 
 function constructGraph(data) {
-	const timeSpent = [];
 	const subreddits = [];
-	for (const key in data) {
-		timeSpent.push(data[key]);
-		subreddits.push(key);
+	const timeSpent = [];
+	for (let i = 0; i < data.length; i++) {
+		subreddits.push(data[i].subreddit);
+		timeSpent.push(data[i].time);
 	}
+	console.log(subreddits, timeSpent);
 
-	// const width = 400;
-	// const barHeight = 20;
-	// const height = barHeight * timeSpent.length;
+	const width = 900;
+	const barHeight = 20;
+	const height = barHeight * timeSpent.length;
 
-	// const chart = d3.select('#chart')
-	// 	.attr('width', width)
-	// 	.attr('height', height);
+	const xScale = d3.scale.linear()
+		.domain([0, d3.max(timeSpent)])
+		.range([0, width]);
 
-	// const xScale = d3.scale.linear()
-	// 	.domain([0, d3.max(timeSpent)])
-	// 	.range([0, width]);
-	// const yScale = d3.scale.ordinal()
-	// 	.domain(d3.range(timeSpent.length))
-	// 	.rangeBands([0, height]);
-	// chart.selectAll('rect')
-	// 	.data(timeSpent)
-	// 	.enter().append('rect')
-	// 	.attr('x', 0)
-	// 	.attr('y', yScale)
-	// 	.attr('width', xScale)
-	// 	.attr('height', yScale.rangeBand());
-}
+	const chart = d3.select('#chart')
+		.attr('width', width)
+		.attr('height', height);
+
+	const bar = chart.selectAll('g')
+		.data(timeSpent)
+		.enter().append('g')
+			.attr('transform', (d, i) => 'translate(0, ' + i * barHeight + ')');
+
+	bar.append('rect')
+		.attr('width', xScale)
+		.attr('height', barHeight - 1);
 
 document.addEventListener('DOMContentLoaded', () => {
 	console.log('hello');
 	const topFive = calculateTopFive(getStats());
-	// constructGraph(topFive);
+	console.log(topFive);
+	constructGraph(topFive);
 });
